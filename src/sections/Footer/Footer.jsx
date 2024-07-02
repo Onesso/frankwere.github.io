d71cd84bfd30abf3ea38bd6f581502e233bac8f5
Frank Onesso <odhiambofrank1965@gmail.com>
2024-07-03 02:38:37 +0300

    used moment to get the exact time

--- src/sections/Footer/Footer.jsx
+++ src/sections/Footer/Footer.jsx
@@ -1,10 +1,14 @@
 import styles from "./FooterStyles.module.css";
+import moment from "moment";
 
 function Footer() {
+  const now = moment();
+  const formatedTime = now.format("MMMM Do YYYY, h:mm:ss a");
   return (
     <section className={styles.container} id="footer">
       <p>
-        &copy; 2024 Frank Were <br /> All rights reserved
+        &copy; Frank Were <br />
+        All rights reserved <br /> {formatedTime}
       </p>
     </section>
   );
