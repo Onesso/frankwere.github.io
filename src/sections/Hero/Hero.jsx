5178ae09c5cd50a5b37722ddeeb0a61319851a14
Frank Onesso <odhiambofrank1965@gmail.com>
2024-07-02 03:10:18 +0300

    added the toggle effect

---
+++ src/sections/Hero/Hero.jsx
@@ -0,0 +1,63 @@
+import React from "react";
+import styles from "./HeroStyles.module.css";
+import heroimg from "../../assets/hero-img.png";
+import sun from "../../assets/sun.svg";
+import moon from "../../assets/moon.svg";
+import xIcon from "../../assets/twitter-light.svg";
+import githubIcon from "../../assets/github-light.svg";
+import linkedinIcon from "../../assets/linkedin-light.svg";
+import cv from "../../assets/cv.pdf";
+import { useTheme } from "../../common/ThemeContext";
+
+function Hero() {
+  const { theme, toggleTheme } = useTheme();
+
+  const themeIcon = theme === "light" ? sun : moon;
+
+  console.log("Current theme:", theme); // Debugging line to check current theme
+
+  return (
+    <section id="hero" className={styles.container}>
+      <div className={styles.colorModeContainer}>
+        <img
+          className={styles.hero}
+          src={heroimg}
+          alt="profile image of Frank were"
+        />
+        
+        <img
+          onClick={toggleTheme}
+          className={styles.colorMode}
+          src={themeIcon}
+          alt="color mode icon"
+        />
+      </div>
+      <div className={styles.info}>
+        <h1>
+          Frank <br /> Were
+        </h1>
+        <h2>Fullstack Developer</h2>
+        <span>
+          <a href="https://x.com/FrankOdhis1" target="_blank" rel="noopener noreferrer">
+            <img src={xIcon} alt="x icon" />
+          </a>
+          <a href="https://github.com/FrankOdhis1" target="_blank" rel="noopener noreferrer">
+            <img src={githubIcon} alt="github icon" />
+          </a>
+          <a href="https://linkedin.com/FrankOdhis1" target="_blank" rel="noopener noreferrer">
+            <img src={linkedinIcon} alt="linkedin icon" />
+          </a>
+        </span>
+        <p>
+          With a passion of developing enterprise applications with React and
+          Django
+        </p>
+        <a href={cv} download>
+          <button className="hover">Resume</button>
+        </a>
+      </div>
+    </section>
+  );
+}
+
+export default Hero;
